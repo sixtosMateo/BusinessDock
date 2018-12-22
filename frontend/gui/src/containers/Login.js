b5fce11a0e2b69c36ead820465c5e5af572f3dce
mateosixtos <sixtosmateo@gmail.com>
2018-12-22 00:13:28 -0800

    setting up my login component and react urls

--- frontend/gui/src/containers/Login.js
+++ frontend/gui/src/containers/Login.js
@@ -4,6 +4,11 @@ import {
 } from 'antd';
 
 
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import * as actions from '../store/actions/auth';
+
+
 // loding icon from ant desin
 const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 
@@ -14,47 +19,71 @@ class NormalLoginForm extends React.Component {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        // console.log('Received values of form: ', values);
+        this.props.onAuth(values.userName, values.password);
+        this.props.history.push('/');
       }
     });
   }
 
   render() {
 
+    let errorMessage = null;
+    if(this.props.error){
+      errorMessage=(
+        <p> {this.props.error.message} </p>
+      );
+    }
+
+    const { getFieldDecorator } = this.props.form;
+
 
 
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
         <FormItem>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-          )}
+            {getFieldDecorator('userName', {
+              rules: [{ required: true, message: 'Please input your username!' }],
+            })(
+              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            )}
         </FormItem>
+
         <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-          )}
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+              )}
         </FormItem>
+
         <FormItem>
-          {getFieldDecorator('remember', {
-            valuePropName: 'checked',
-            initialValue: true,
-          })(
-            <Checkbox>Remember me</Checkbox>
-          )}
-          <a className="login-form-forgot" href="">Forgot password</a>
-          <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
-          </Button>
-          Or <a href="">register now!</a>
+                <Button type="primary" htmlType="submit" style={{marginRight: '10px'}}>
+                    Login
+                </Button>
+
         </FormItem>
-      </Form>
+        </Form>
     );
   }
 }
 
 const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
+
+const mapStateToProps= (state) =>{
+  return{
+    loading: state.loading,
+    error: state.error
+  }
+
+}
+
+
+const mapDispatchToProps = dispatch =>{
+  return {
+    onAuth: (username, password)=> dispatch(actions.authLogin(username, password))
+  }
+}
+
+
+export default connect(mapStateToProps,  mapDispatchToProps)(WrappedNormalLoginForm);
--- frontend/gui/src/routes.js
+++ frontend/gui/src/routes.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-// import Login from './containers/Login';
+import Login from './containers/Login';
 
 
 const BaseRouter = () =>(
   <div>
 
-
-
+    <Route exact path='/login/' component={Login}/>{" "}
   </div>
 
 );
