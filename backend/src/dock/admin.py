404af8c150e6077b3f09ae20acf3b942927205b3
mateosixtos <sixtosmateo@gmail.com>
2018-12-21 20:35:47 -0800

    initializing Item Model

--- backend/src/dock/admin.py
+++ backend/src/dock/admin.py
@@ -14,3 +14,31 @@ class Employee(models.Model):
 
 
     createdAt = models.DateTimeField(default=timezone.now)
+
+
+class Item(models.Models):
+    itemId = models.AutoField(primary_key=True)
+    barcode = models.CharField(unique=True, max_length=30, default=None)
+    name = models.CharField(max_length = 30, default=None)
+    inStockQty = models.IntegerField(default = 0, blank=True)
+    picture = models.CharField(max_length = 30, default=None)
+    color = models.CharField(max_length = 30, default=None)
+    ageRequirement = models.CharField(max_length = 30, default=None)
+    purchasedPrice = models.FloatField(default = 0.0, blank=True)
+    salePrice = models.FloatField(default = 0.0, blank=True)
+    department = models.CharField(max_length = 30, default=None)
+    createdAt = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "item"
+
+    def save(self, *args, **kwargs):
+        print('Item save() is called.')
+        super(Item, self).save(using='')
+
+    def __unicode__(self):
+        return "{0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} ".format(
+            self.itemId, self.name, self.inStockQty, self.picture, self.color,
+            self.ageRequirement, self.purchasedPrice, self.salePrice,
+            self.department, self.barcode,
+            self.createdAt)
