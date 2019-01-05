1b3356cd43cd41b1cf6de196c22c20c9e39401c5
mateosixtos <sixtosmateo@gmail.com>
2019-01-05 13:21:36 -0800

    displayed emlpoyees using ant-design cards

---
+++ frontend/gui/src/components/EmployeeAvatar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { List, Avatar, Icon } from 'antd';
+
+
+
+
+const IconText = ({ type, text }) => (
+  <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+    {text}
+  </span>
+);
+
+const EmployeeAvatar =(props)=>{
+  return(
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 3,
+        }}
+        dataSource={props.data}
+
+        renderItem={item => (
+          <List.Item
+            key={item.employeeId}
+            actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
+            extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
+          >
+            <List.Item.Meta
+              title={item.employeeId}
+              description={item.employmentType}
+            />
+            {item.storeId}
+          </List.Item>
+        )}
+      />
+
+
+  );
+}
+
+export default EmployeeAvatar;
