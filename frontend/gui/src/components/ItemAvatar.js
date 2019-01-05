b1175e0a0a1e85af018af5fccf3def6e46ab2535
mateosixtos <sixtosmateo@gmail.com>
2019-01-05 14:50:04 -0800

    displays all items with search functionality

---
+++ frontend/gui/src/components/ItemAvatar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { List, Avatar, Icon } from 'antd';
+
+const IconText = ({ type, text }) => (
+  <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+    {text}
+  </span>
+);
+
+const ItemAvatar =(props)=>{
+  return(
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 3,
+        }}
+        dataSource={props.data}
+
+        renderItem={item => (
+          <List.Item
+            key={item.barcode}
+            actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
+            extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
+          >
+            <List.Item.Meta
+              title={item.barcode}
+              description={item.inStockQty}
+            />
+            {item.name}
+          </List.Item>
+        )}
+      />
+
+
+  );
+}
+
+export default ItemAvatar;
--- frontend/gui/src/components/Items.js
+++ frontend/gui/src/components/Items.js
@@ -1,46 +1,99 @@
+// bug: componentWillReceiveProps(props) is being called twice therefore two calls to server
+// create a cached that check whether the query was called or not
+
 import React from 'react';
-import { List, Avatar, Icon } from 'antd';
+import axios from 'axios';
+import { List, Input, Breadcrumb, Avatar, Icon} from 'antd';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by'
+
+import ItemAvatar from './ItemAvatar';
 
 
-const IconText = ({ type, text }) => (
-  <span>
-    <Icon type={type} style={{ marginRight: 8 }} />
-    {text}
-  </span>
-);
+const Search = Input.Search;
 
-const Items =(props)=>{
-  return(
-      <List
-        itemLayout="vertical"
-        size="large"
-        pagination={{
-          onChange: (page) => {
-            console.log(page);
-          },
-          pageSize: 3,
-        }}
+class Items extends React.Component{
 
-        dataSource={props.data}
+  constructor(props) {
+       super(props);
+       // this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
+       this.items = this.componentWillReceiveProps(props);
+     }
 
-        renderItem={item => (
-          <List.Item
-            key={item.name}
-            actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
-            extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
-          >
-            <List.Item.Meta
-              avatar={<Avatar src={item.avatar} />}
-              title={<a href={`/`}>{item.name}</a>}
-              description={item.barcode}
-            />
-            {item.name}
-          </List.Item>
-        )}
-      />
+  state ={
+    items:[],
+    query:  ''
+  }
 
-
-  );
+  updateQuery=(query)=>{
+  this.setState({
+    query: query.trim()
+  })
 }
 
-export default Items;
+  componentWillReceiveProps(newProps){
+
+    if(newProps.token){
+      axios.defaults.headers = {
+         "Content-Type": "application/json",
+         Authorization: newProps.token,
+
+      }
+
+      axios.get('http://127.0.0.1:8000/api/items/')
+        .then(res => {
+          this.setState({
+            items: res.data
+          });
+        })
+    }
+
+
+  }
+
+  render(){
+    let showingItems
+
+      if(this.state.query){
+        const match = new RegExp(escapeRegExp(this.state.query), 'i')
+
+        showingItems = this.state.items.filter((item) =>
+        match.test(item.barcode))
+      }
+      else{
+        showingItems = this.state.items
+      }
+
+      showingItems.sort(sortBy('itemId'))
+
+      return(
+
+        <div className="ItemComponent">
+        <Search
+          placeholder="Search Item"
+          value={this.state.query}
+          onChange={(event) => this.updateQuery(event.target.value)}
+          />
+
+
+          <ItemAvatar data={showingItems} />
+        </div>
+      )
+
+  }
+
+
+}
+
+
+const mapStateToProps = state =>{
+  // return object is what you want to map into a property
+  return {
+    token: state.token
+  }
+}
+
+
+export default withRouter(connect(mapStateToProps)(Items));
