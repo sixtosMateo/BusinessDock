94f61aaa0faf0abb84a5580430b3e9315b7aaff0
mateosixtos <sixtosmateo@gmail.com>
2019-01-09 14:04:14 -0800

    VendorAvatar component display Vendor data

---
+++ frontend/gui/src/components/VendorAvatar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { List, Avatar, Icon } from 'antd';
+
+
+const IconText = ({ type, text }) => (
+  <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+    {text}
+  </span>
+);
+
+const VendorAvatar =(props)=>{
+  return(
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 3,
+        }}
+        dataSource={props.data}
+
+        renderItem={item => (
+          <List.Item
+            key={item.vendorId}
+            actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
+            extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
+          >
+            <List.Item.Meta
+              title={item.name}
+              description={item.phoneNumber}
+            />
+            {item.hoursOpen}
+          </List.Item>
+        )}
+      />
+
+
+  );
+}
+
+export default VendorAvatar;
