a46d0601643c3a0f887679bb09c8039cacd5ed13
mateosixtos <sixtosmateo@gmail.com>
2019-03-08 18:46:03 -0800

    avatar for each transaction and its data

---
+++ frontend/gui/src/components/avatar/EmployeeOutgoingAvatar.js
@@ -0,0 +1,48 @@
+//item.email
+//item.birthDate
+//item.age
+import React from 'react';
+import { List, Icon, Row, Col } from 'antd';
+import {Link} from 'react-router-dom';
+
+
+const EmployeeOutgoingAvatar =(props)=>{
+  return(
+      <List
+        itemLayout="vertical"
+        size="medium"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 6,
+        }}
+        dataSource={props.data}
+
+        renderItem={item => (
+          <List.Item
+            key={item.transactionId}
+            actions={[]}
+            extra={[]}
+          >
+            <List.Item.Meta/>
+            <Row type="flex" justify="start" style={{textAlign:"center"}}>
+              <Col span={2} style={{width:"17%"}}><strong>{item.transactionId}</strong></Col>
+              <Col span={2} style={{width:"16%"}}><strong>{item.storeId}</strong></Col>
+              <Col span={2} style={{width:"16%"}}><strong>{item.subtotal}</strong></Col>
+              <Col span={2} style={{width:"17%"}}><strong>{item.tax}</strong></Col>
+              <Col span={2} style={{width:"17%"}}><strong>{item.subtotal}</strong></Col>
+              <Col span={2} style={{width:"17%"}}><strong>{item.createdAt}</strong></Col>
+            </Row>
+
+          </List.Item>
+        )}
+      />
+
+
+  );
+}
+
+
+
+export default EmployeeOutgoingAvatar;
